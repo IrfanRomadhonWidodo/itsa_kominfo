8694904689e77a7a9558e041dea713a10d57f93b
ebiina <essay.mukti@mhs.unsoed.ac.id>
2025-07-08 08:12:49 +0700

    halaman profile no nofitikasi dan aktivitas

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -50,19 +50,13 @@
         </div>
 
         <!-- Navigation Tabs -->
-        <div class="border-b border-gray-200 mb-8">
+        {{-- <div class="border-b border-gray-200 mb-8">
             <nav class="-mb-px flex space-x-8">
                 <a href="#" class="border-[#016DAE] text-[#016DAE] whitespace-nowrap py-3 px-1 border-b-2 font-medium text-sm">
                     Profil Saya
                 </a>
-                <a href="#" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-3 px-1 border-b-2 font-medium text-sm">
-                    Aktivitas
-                </a>
-                <a href="#" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-3 px-1 border-b-2 font-medium text-sm">
-                    Notifikasi
-                </a>
             </nav>
-        </div>
+        </div> --}}
 
         <!-- Profile Sections -->
         <div class="space-y-6">
