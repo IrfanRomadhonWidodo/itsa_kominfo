bc83d1d9dc255330ca74c1dfa9dd32bc7ec3864d
ebiina <essay.mukti@mhs.unsoed.ac.id>
2025-07-09 13:26:25 +0700

    tung tung tung sahur

--- app/Http/Controllers/NotifikasiController.php
+++ app/Http/Controllers/NotifikasiController.php
@@ -15,7 +15,6 @@ class NotifikasiController extends Controller
     {
         $notifikasi = Notifikasi::where('user_id', Auth::id())
                                ->with('feedback')
-                               ->with('formulir')
                                ->latest()
                                ->paginate(10);
 
